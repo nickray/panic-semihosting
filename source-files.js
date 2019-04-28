e93efdf530c55b348cca3f9c642368144d6119a9
Travis CI User <travis@example.org>
2019-04-28 08:36:13 +0000

    Update documentation

---
+++ source-files.js
@@ -0,0 +1,13 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["aligned"] = {"name":"","files":["lib.rs","sealed.rs"]};
+sourcesIndex["as_slice"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["bare_metal"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["cortex_m"] = {"name":"","dirs":[{"name":"peripheral","files":["cbp.rs","cpuid.rs","dcb.rs","dwt.rs","fpb.rs","fpu.rs","itm.rs","mod.rs","mpu.rs","nvic.rs","scb.rs","syst.rs","tpiu.rs"]},{"name":"register","files":["apsr.rs","basepri.rs","basepri_max.rs","control.rs","faultmask.rs","lr.rs","mod.rs","msp.rs","pc.rs","primask.rs","psp.rs"]}],"files":["asm.rs","interrupt.rs","itm.rs","lib.rs","macros.rs"]};
+sourcesIndex["cortex_m_semihosting"] = {"name":"","files":["debug.rs","export.rs","hio.rs","lib.rs","macros.rs","nr.rs"]};
+sourcesIndex["generic_array"] = {"name":"","files":["arr.rs","functional.rs","hex.rs","impls.rs","iter.rs","lib.rs","sequence.rs"]};
+sourcesIndex["panic_semihosting"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["stable_deref_trait"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["typenum"] = {"name":"","files":["array.rs","bit.rs","int.rs","lib.rs","marker_traits.rs","operator_aliases.rs","private.rs","type_operators.rs","uint.rs"]};
+sourcesIndex["vcell"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["volatile_register"] = {"name":"","files":["lib.rs"]};
+createSourceSidebar();
